e2341e636f596a30d650d3474cb2b168382a8c64
si-ro <boolean@zpost.plala.or.jp>
2014-08-19 02:19:41 +0900

    append javafx dependency.

---
+++ NyARToolkit.sample.java3d.webcam-capture/src/main/java/jp/qualitas/nyartoolkit/java3d/webcam/sample/NyARJava3DfxController.java
@@ -0,0 +1,15 @@
+package jp.qualitas.nyartoolkit.java3d.webcam.sample;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class NyARJava3DfxController implements Initializable {
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		
+	}
+
+}

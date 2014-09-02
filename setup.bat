bec60bf61feaeee1b657c243b940328f78eec30d
sakaguchi manabu <sakaguchi.manab@jp.fujitsu.com>
2014-09-02 22:42:06 +0900

    create maven setup bat file.

--- setup.bat
+++ setup.bat
@@ -1,6 +1,3 @@
-REM set PATH=C:\apache-maven-3.1.1\bin;%PATH%
-REM mvn eclipse:configure-workspace -Declipse.workspace=D:\c3dev\eclipse\workspace
-cd C:\Users\sakaguchi\git\NyARToolkit
 call mvn eclipse:eclipse -f NyARToolkit.utils.webcam-capture\pom.xml
 call mvn eclipse:eclipse -f NyARToolkit.utils.j2se\pom.xml
 call mvn eclipse:eclipse -f NyARToolkit.utils.java3d.webcam-capture\pom.xml

22d63d9f0b851e7efba9ab45ca0d9dc49e15fb65
si-ro <boolean@zpost.plala.or.jp>
2015-01-05 00:08:46 +0900

    typo.

--- setup.bat
+++ setup.bat
@@ -2,4 +2,4 @@ call mvn eclipse:eclipse -f NyARToolkit.utils.webcam-capture\pom.xml
 call mvn eclipse:eclipse -f NyARToolkit.utils.j2se\pom.xml
 call mvn eclipse:eclipse -f NyARToolkit.utils.java3d.webcam-capture\pom.xml
 call mvn eclipse:eclipse -f NyARToolkit.utils.java3d.RaspberryPI\pom.xml
-call mvn eclipse:eclipse -f NyARToolkit.RapsberryPI.sample\pom.xml
+call mvn eclipse:eclipse -f NyARToolkit.RaspberryPI.sample\pom.xml
